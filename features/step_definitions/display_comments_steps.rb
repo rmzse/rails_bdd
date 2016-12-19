74a93a23ca9aef7b35956f3aa0eb26dd08ae8d95
rmzse <rodrigo@therawdelicious.com>
2016-12-19 08:52:05 +0100

    try for an hour and a half how to get the id of an article with a certain title and fail to match the resources route for the article show action based on article id

---
+++ features/step_definitions/display_comments_steps.rb
@@ -0,0 +1,17 @@
+When(/^I am on the "([^"]*)" article page$/) do |article_title|
+	# find the article id of the article with article_title
+	@article = Article.select{|article| article.title == "Build rails stuff"}
+	visit 'articles/:@article.id'
+end
+
+Then(/^I should see a comment with the title "([^"]*)"$/) do |arg1|
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a comment with the body "([^"]*)"$/) do |arg1|
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a commenter email adress "([^"]*)"$/) do |arg1|
+	pending # Write code here that turns the phrase above into concrete actions
+end
