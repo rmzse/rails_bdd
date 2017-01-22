f1f0decb785305696e3623a921844b6dc7cb8007
rmzse <rodrigo@therawdelicious.com>
2017-01-23 00:41:35 +0100

    change route to helper method, test failing

--- features/step_definitions/display_comments_steps.rb
+++ features/step_definitions/display_comments_steps.rb
@@ -1,7 +1,9 @@
 When(/^I am on the "([^"]*)" article page$/) do |article_title|
 	# find the article id of the article with article_title
-	@article = Article.select{|article| article.title == article_title}
-	visit '/articles/:id'
+	# @article = Article.select{|article| article.title == article_title}
+	# visit '/articles/:id'
+	article = Article.all.find_by(title: "#{article_title}")
+	expect(current_path).to eq article_path(article.id)
 end
 
 Then(/^I should see a comment with the title "([^"]*)"$/) do |arg1|
